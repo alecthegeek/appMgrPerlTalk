ecb7e870c2309a851a73d5693cdba5c4b1f4a534
Alec Clews <alec.clews@voga.com.au>
2011-05-30 17:21:06 +1000

    Added a mindmap

---
+++ notes/Application Management with Perl.mm
@@ -0,0 +1,39 @@
+<map version="0.9.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Application Management with Perl" ID="ID_733753105" CREATED="1306738457002" MODIFIED="1306738480999">
+<hook NAME="MapStyle" max_node_width="600"/>
+<node TEXT="Agenda" POSITION="right" ID="ID_1615644507" CREATED="1306738483091" MODIFIED="1306738504309">
+<node TEXT="What needs to be done" ID="ID_1570668514" CREATED="1306738506370" MODIFIED="1306738865312">
+<node TEXT="Scope" ID="ID_1071690094" CREATED="1306738843137" MODIFIED="1306738846798">
+<node TEXT="Applications requireing large numbers of configuration settings" ID="ID_319662808" CREATED="1306738909314" MODIFIED="1306738930609"/>
+</node>
+<node TEXT="Issues" ID="ID_1422549403" CREATED="1306738847210" MODIFIED="1306738849205">
+<node TEXT="Errors" ID="ID_487412003" CREATED="1306738933202" MODIFIED="1306738939091"/>
+<node TEXT="Compliance" ID="ID_1501002310" CREATED="1306738940527" MODIFIED="1306738950291"/>
+</node>
+<node TEXT="Problems" ID="ID_156317865" CREATED="1306738849941" MODIFIED="1306738857705">
+<node TEXT="Not planned for" ID="ID_58640183" CREATED="1306739009291" MODIFIED="1306739016594"/>
+<node TEXT="It&apos;s a new approach" ID="ID_71984805" CREATED="1306739898303" MODIFIED="1306739904929"/>
+<node TEXT="time and money" ID="ID_1310479077" CREATED="1306739909030" MODIFIED="1306739914124"/>
+</node>
+</node>
+<node TEXT="Technical barriers to be overcome" ID="ID_1544872049" CREATED="1306738513663" MODIFIED="1306738876765">
+<node TEXT="Security" ID="ID_870434223" CREATED="1306738539839" MODIFIED="1306738579559"/>
+<node TEXT="User Interfaces" ID="ID_1903819288" CREATED="1306738544239" MODIFIED="1306738584712"/>
+<node TEXT="Increasing Complexity" ID="ID_681240641" CREATED="1306738546103" MODIFIED="1306738601593"/>
+<node TEXT="Software Maintaince" ID="ID_422791412" CREATED="1306738552145" MODIFIED="1306738612018"/>
+<node TEXT="Data Integrity" ID="ID_788706175" CREATED="1306738554643" MODIFIED="1306738620136"/>
+<node TEXT="Error Recovery" ID="ID_1257798126" CREATED="1306738556831" MODIFIED="1306738636902"/>
+<node TEXT="Business Rules" ID="ID_375427799" CREATED="1306738561747" MODIFIED="1306738643196"/>
+</node>
+<node TEXT="Data Storage and maintenance." ID="ID_1508726033" CREATED="1306738681482" MODIFIED="1306738683448"/>
+<node TEXT="Useful CPAN Modules for: Templating; Data file access; Database&#xa;access; Logging; Email; Password generation; etc." ID="ID_1922527064" CREATED="1306738692784" MODIFIED="1306738694791"/>
+<node TEXT="How to structure the final solution for ease of extension,&#xa;installation and maintenance." ID="ID_723804549" CREATED="1306738702471" MODIFIED="1306738704130"/>
+<node TEXT="Installing your own Perl and CPAN modules on restricted environments" ID="ID_1846198357" CREATED="1306738704800" MODIFIED="1306738725954"/>
+</node>
+<node TEXT="Summary" POSITION="left" ID="ID_220348280" CREATED="1306738750893" MODIFIED="1306738754409">
+<node TEXT="How to use Perl and associated tools to manage large scale application&#xa;environments when Puppet, Chef etc cannot be used. Includes a look at&#xa;the technical issues to be solved, useful CPAN modules, how to maintain&#xa;the data and how to structure the resulting framework and scripts.&#xa;&#xa;This presentation is intended for technical application environment&#xa;managers who want to adopt an &#x201c;infrastructure as code&#x201d; approach, but do&#xa;not have access to the appropriate tools to assist them. Every&#xa;environment has different technical needs to solve. However attendees&#xa;should, after listening to the talk, have a good starting point on how&#xa;to approach these issues in their own organisations, no matter what&#xa;scripting language they use&#xa;&#xa;The devops community has brought best practice use of automation tools&#xa;to the forefront of modern agile software delivery teams. However there&#xa;are a large number of environments where such tools are not available&#xa;for a variety reasons. Based on experience at a large financial&#xa;institution this paper looks at a &#x201c;roll your own&#x201d; approach when tools&#xa;like Puppet or Chef cannot be used. The problem solved is the set-up of&#xa;a large, complex, application and the presentation breaks the the&#xa;solution down into six major topics, some of which will be covered at a&#xa;high level because of time constraints." ID="ID_306301196" CREATED="1306738759883" MODIFIED="1306738762920"/>
+</node>
+<node TEXT="Next Steps" POSITION="right" ID="ID_1435979022" CREATED="1306738796680" MODIFIED="1306738810537"/>
+</node>
+</map>
